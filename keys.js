607c3486477cc126972e89a614ca83c93df800e0
AKSHAT <akthd4@gmail.com>
2020-11-03 22:53:05 +0530

    Heroku 2

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+     MONGODBURI: 'mongodb+srv://akshatatray:akshat102413atray@miniproject.2x652.mongodb.net/MiniProject?retryWrites=true&w=majority',
+     JWTSECRET: "miniaksvarpro"
+}
